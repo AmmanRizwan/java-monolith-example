138dc29abc25175a3a30aa8488136061c7afa916
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-10-16 23:54:24 +0530

    Implementing the Customer entity for relational database

---
+++ src/main/java/com/monolith/example/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.monolith.example.repository;
+
+import com.monolith.example.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+}
